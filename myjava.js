067fb6bdff2dd25cb47aed8d6be71bc02bdb69d5
Chi-Chidev <fabulousesther05@gmail.com>
2024-08-13 16:19:28 +0100

    renamed myjava page to index page

--- myjava.js
+++ myjava.js
@@ -1,20 +1,30 @@
 
 document.getElementById('decreaseByOne').onclick = function () {
-    let output = document.getElementById('displayOne');
-    output.innerText = Number(output.innerText) - 1;
+    // let output = document.getElementById('displayOne');
+    // output.innerText = Number(output.innerText) - 1;
+    document.getElementById('displayOne').innerText = Number(document.getElementById('displayOne').innerText) - 1;
 };
 
 document.getElementById('increaseByOne').onclick = function () {
-    let output = document.getElementById('displayOne');
-    output.innerText = Number(output.innerText) + 1;
+    // let output = document.getElementById('displayOne');
+    // output.innerText = Number(output.innerText) + 1;
+    document.getElementById('displayOne').innerText = Number(document.getElementById('displayOne').innerText) + 1;
 };
 
 document.getElementById('increaseByTwo').onclick = function () {
-    let counter = document.getElementById('displayTwo');
-    counter.innerText = Number(counter.innerText) + 2;
+    // let counter = document.getElementById('displayTwo');
+    // counter.innerText = Number(counter.innerText) + 2;
+document.getElementById('displayOne').innerText = Number(document.getElementById('displayOne').innerText) + 2;
 };
 
 document.getElementById('decreaseByTwo').onclick = function () {
-    let counter = document.getElementById('displayTwo');
-    counter.innerText = Number(counter.innerText) - 2;
+    // let counter = document.getElementById('displayTwo');
+    // counter.innerText = Number(counter.innerText) - 2;
+    document.getElementById('displayOne').innerText = Number(document.getElementById('displayOne').innerText) - 2;
+};
+
+document.getElementById('butt').onclick = function () {
+ document.getElementById('text').innerHTML = "It is done it" ;
+// let display =  document.getElementById('text');
+// display.innerText = "it is";
 };
